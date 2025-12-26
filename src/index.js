3e3f2b37d4689a1e1e7e6491d46ff02eb760d784
keerthu2712 <m.keerthu2712@gmail.com>
2025-12-26 21:58:47 +0530

    fix: replace BrowserRouter with HashRouter for improved routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
