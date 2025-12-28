677b78ca9533a1e763e202d3f2531f42b3438e37
keerthu2712 <m.keerthu2712@gmail.com>
2025-12-28 19:34:43 +0530

    fix: replace HashRouter with BrowserRouter for routing consistency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -8,8 +8,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
